3a19b617cf7415257cce54772a832623074a156c
Harry Garrood <harry@garrood.me>
2014-04-14 20:16:01 +0100

    Publish images too

--- publish.sh
+++ publish.sh
@@ -3,20 +3,36 @@ BASE_URL="http://localhost:8000"
 MAIN="/Main.elm"
 MAIN_HTML="/Main.html"
 RUNTIME="/elm-runtime.js"
+IMGS="img"
+
 PUBLISH_DIR=".publish.sh"
 HOST="buzzard.garrood.me"
 REMOTE_BASE_PATH="~/code/library-evac"
 
 get() {
-    mkdir -p "$PUBLISH_DIR"
-    curl "${BASE_URL}${MAIN}"    > "${PUBLISH_DIR}${MAIN_HTML}"
-    curl "${BASE_URL}${RUNTIME}" > "${PUBLISH_DIR}${RUNTIME}"
+    mkdir -p "$PUBLISH_DIR/$IMGS"
+    echo "putting things into publish dir..."
+    p_indent "$MAIN"
+    curl "${BASE_URL}${MAIN}"    2>/dev/null > "${PUBLISH_DIR}${MAIN_HTML}"
+    p_indent "$RUNTIME"
+    curl "${BASE_URL}${RUNTIME}" 2>/dev/null > "${PUBLISH_DIR}${RUNTIME}"
+    for f in $IMGS/*; do
+        p_indent "$f"
+        cp "$f" "${PUBLISH_DIR}/$f"
+    done
 }
 
 send() {
-    for f in "$MAIN_HTML" "$RUNTIME"; do
-        scp "${PUBLISH_DIR}${f}" "${HOST}:${REMOTE_BASE_PATH}${f}"
+    echo "copying to remote host..."
+    for f in `find "$PUBLISH_DIR" -type f`; do
+        g="${f#$PUBLISH_DIR}"
+        p_indent "$g"
+        scp "$f" "${HOST}:${REMOTE_BASE_PATH}${g}"
     done
+}
+
+p_indent() {
+    echo "   $1"
 }
 
 get
