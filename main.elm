71be97bb76110f2c572c9b9736335b16e5962e83
Harry Garrood <harry@garrood.me>
2014-04-14 02:48:44 +0100

    Fix movement

--- main.elm
+++ main.elm
@@ -1,30 +1,54 @@
 import Keyboard
-import Window
 
--- COLOURS
 colours = { tan=rgb 255 190 120, blue=rgb 12 100 200, red=red}
 
--- MODEL
+data Dir = Left | Right | Up | Down
 data FloorTile = Floor
-tileSize = 36
+tileSize = 32
+
+type Positioned a = { a | x:Int, y:Int }
+
+toPositioned : Dir -> Positioned {}
+toPositioned d = case d of
+    Left  -> { x=-1, y=0  }
+    Right -> { x=1,  y=0  }
+    Up    -> { x=0,  y=1  }
+    Down  -> { x=0,  y=-1 }
+
+fromPositioned : Positioned {} -> Maybe Dir
+fromPositioned {x,y} = if | x /= 0    -> Just <| if x > 0 then Right else Left
+                          | y /= 0    -> Just <| if y > 0 then Up    else Down
+                          | otherwise -> Nothing
+
+vecAdd : Positioned a -> Positioned b -> Positioned b
+a `vecAdd` b = { b | x <- a.x + b.x,
+                     y <- a.y + b.y }
 
 -- Should both be odd (for now)
 floorDimensions : { x:Int, y:Int }
-floorDimensions = { x=15, y=15 }
+floorDimensions = { x=29, y=19 }
 
 floorSize = { x=floorDimensions.x * tileSize, y=floorDimensions.y * tileSize }
 
 type Floor = [[FloorTile]]
+
+floor : Floor
 floor = repeat floorDimensions.x (repeat floorDimensions.y Floor)
 
-initialPlayer : { x:Int, y:Int }
-initialPlayer = { x=0, y=0 }
+data Player = Player (Positioned {})
 
+initialPlayer : Player
+initialPlayer = Player { x=0, y=0 }
+
+renderFloor : Floor -> Element
 renderFloor f =
   let cols = f
   in  flow right (map renderCol cols)
 
+renderCol : [FloorTile] -> Element
 renderCol col = flow down (map toElement col)
+
+toElement : FloorTile -> Element
 toElement tile =
   let
     sh = square tileSize
@@ -34,20 +58,37 @@ toElement tile =
     , sh |> outlined (solid colours.blue)
     ]
 
+renderPlayer : Player -> Form
 renderPlayer player = filled colours.red (square tileSize)
 
-getCollageCoords {x,y} =
+getCollageCoords : Player -> (Float, Float)
+getCollageCoords (Player {x,y}) =
    let translate length = (\v -> v - (length - 1) `div` 2)
-       adjust length v = v |> translate length |> (\v -> v * tileSize) |> toFloat
+       adjust length v = v |> translate length
+                           |> (\v -> v * tileSize)
+                           |> toFloat
    in (adjust floorDimensions.x x, adjust floorDimensions.y y)
 
-step input player = { x=player.x + input.x, y=player.y + input.y }
+moveDir : Dir -> Positioned a -> Positioned a
+moveDir dir pos = toPositioned dir `vecAdd` pos
 
-signalPlayer = foldp step initialPlayer Keyboard.arrows
+step : Dir -> Player -> Player
+step input (Player rec) = Player <| moveDir input rec
 
-renderWorld player =collage floorSize.x floorSize.y
+step' : Maybe Dir -> Player -> Player
+step' mdir p = case mdir of
+    Just dir -> step dir p
+    Nothing  -> p
+
+input : Signal (Maybe Dir)
+input = keepIf isJust (Just Right) (fromPositioned <~ Keyboard.arrows)
+
+sPlayer : Signal Player
+sPlayer = foldp step' initialPlayer input
+
+renderWorld player = collage floorSize.x floorSize.y
   [ toForm <| renderFloor floor
   , renderPlayer player |> move (getCollageCoords player)
   ]
 
-main = renderWorld <~ signalPlayer
+main = renderWorld <~ sPlayer
